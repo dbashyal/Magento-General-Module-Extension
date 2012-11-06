a30e886fc332d923aec82774a0f8ecb8b0e08551
damu <damodar@acidgreen.com.au>
2012-11-06 11:30:25 +1100

    first commit with general helper

---
+++ app/code/community/Technooze/Tgeneral/Helper/Data.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Technooze_Tgeneral extension
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Technooze
+ * @package    Technooze_Tgeneral
+ * @copyright  Copyright (c) 2008 Technooze LLC
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Product View block
+ *
+ * @category Technooze
+ * @package  Technooze_Tgeneral
+ * @module   Tgeneral
+ * @author   Damodar Bashyal (enjoygame @ hotmail.com)
+ */
+class Technooze_Tgeneral_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    public function getFreeReturnsUrl()
+   	{
+   		return $this->_getUrl('free-returns');
+   	}
+
+    public function getFreeShippingUrl()
+   	{
+   		return $this->_getUrl('free-shipping');
+   	}
+
+    public function getStorePhoneNumber()
+   	{
+   		return Mage::getStoreConfig('general/store/information_phone');
+   	}
+}
