d43564fe35c1b89b64aea93bb02d302f01676a85
damu <damodar@acidgreen.com.au>
2013-01-31 18:37:12 +1100

    added get current category function so it can be fetched from current product too when required

--- app/code/community/Technooze/Tgeneral/Helper/Data.php
+++ app/code/community/Technooze/Tgeneral/Helper/Data.php
@@ -46,6 +46,24 @@ class Technooze_Tgeneral_Helper_Data extends Mage_Core_Helper_Abstract
 
     public function getStoreAddress()
    	{
-   		return Mage::getStoreConfig('general/store_information/address');
+        return Mage::getStoreConfig('general/store_information/address');
+   	}
+
+    public function getCurrentCategory()
+   	{
+        $current_category = Mage::registry('current_category');
+        if(!is_object($current_category))
+        {
+            $current_product = Mage::registry('current_product');
+            if(is_object($current_product))
+            {
+                $categories = $current_product->load($current_product->getId())->getCategoryIds();
+                if (is_array($categories) and count($categories))
+                {
+                    $current_category = Mage::getModel('catalog/category')->load($categories[0]);
+                }
+            }
+        }
+        return $current_category;
    	}
 }
