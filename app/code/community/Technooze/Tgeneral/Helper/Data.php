e43b3fb306b1cacd7061eabaadbc2575b7176711
damu <damodar@acidgreen.com.au>
2013-05-08 12:50:34 +1000

    added new function to add new images to a product in magento

--- app/code/community/Technooze/Tgeneral/Helper/Data.php
+++ app/code/community/Technooze/Tgeneral/Helper/Data.php
@@ -90,6 +90,85 @@ class Technooze_Tgeneral_Helper_Data extends Mage_Core_Helper_Abstract
     /**
      *
      */
+    public function addImagesToProduct($_productId = 0, $images = array())
+    {
+        if(!$_productId) return $this;
+        $product = Mage::getModel('catalog/product')->load($_productId);
+
+        $importData = array();
+        $images = array_filter($images);
+        if(empty($images)) return $product;
+
+        $img = array_shift($images);
+        $img = DS . trim(str_replace(array('/', '\\'), DS, trim($img)), DS);
+        $importData['image'] = $importData['small_image'] = $importData['thumbnail'] = $img;
+
+         /*
+          Code for image upload in version 1.5.x.x and above
+         */
+         if(!$this->_mediaGalleryBackendModel){
+             $this->_mediaGalleryBackendModel = Mage::getModel('catalog/product')->getResource()->getAttribute('media_gallery')->getBackend();
+         }
+         $mediaGalleryBackendModel = $this->_mediaGalleryBackendModel;
+
+        $arrayToMassAdd = array();
+
+        foreach ($product->getMediaAttributes() as $mediaAttributeCode => $mediaAttribute) {
+            if (isset($importData[$mediaAttributeCode])) {
+                $file = $importData[$mediaAttributeCode];
+                if (is_file(Mage::getBaseDir('media') . DS . 'import' . $file) && !$mediaGalleryBackendModel->getImage($product, $file)) {
+                    $arrayToMassAdd[] = array('file' => trim($file), 'mediaAttribute' => $mediaAttributeCode);
+                }
+            }
+        }
+
+        //Mage::log($arrayToMassAdd);
+
+        $addedFilesCorrespondence =
+            $mediaGalleryBackendModel->addImagesWithDifferentMediaAttributes($product, $arrayToMassAdd, Mage::getBaseDir('media') . DS . 'import', false, false);
+        //Mage::log('pass');
+
+        foreach ($product->getMediaAttributes() as $mediaAttributeCode => $mediaAttribute) {
+            $addedFile = '';
+            if (isset($importData[$mediaAttributeCode . '_label'])) {
+                $fileLabel = trim($importData[$mediaAttributeCode . '_label']);
+                if (isset($importData[$mediaAttributeCode])) {
+                    $keyInAddedFile = array_search($importData[$mediaAttributeCode],
+                        $addedFilesCorrespondence['alreadyAddedFiles']);
+                    if ($keyInAddedFile !== false) {
+                        $addedFile = $addedFilesCorrespondence['alreadyAddedFilesNames'][$keyInAddedFile];
+                    }
+                }
+
+                if (!$addedFile) {
+                    $addedFile = $product->getData($mediaAttributeCode);
+                }
+                if ($fileLabel && $addedFile) {
+                    $mediaGalleryBackendModel->updateImage($product, $addedFile, array('label' => $fileLabel));
+                }
+            }
+        }
+
+        foreach($images as $img){
+            $img = DS . trim(str_replace(array('/', '\\'), DS, $img), DS);
+            $image_path = realpath(Mage::getBaseDir('media') . DS . 'import' . $img);
+            if(file_exists($image_path) && is_file($image_path)){
+                $product->addImageToMediaGallery(Mage::getBaseDir('media') . DS . 'import' . $img, null, false, false);
+            }
+        }
+
+        try {
+            $product->save();
+        } catch (Exception $e){
+            Mage::logException($e);
+        }
+
+        return $product;
+    }
+
+    /**
+     *
+     */
     public function removeImagesFromProduct($_productId = 0)
     {
         if(!$_productId) return $this;
